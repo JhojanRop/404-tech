8ddb05e99c9070a4e8441b8a5cebe584fc781120
Jhojan Ropero <jhojanropero0@gmail.com>
2025-05-30 01:51:28 -0500

    feat: Product type definition

---
+++ src/types/product.d.ts
@@ -0,0 +1,15 @@
+export interface Product {
+  id: number | string;
+  title: string;
+  description?: string;
+  price: number;
+  onSale?: boolean;
+  discountPercentage?: number;
+  oldPrice?: number;
+  rating?: number;
+  category?: string[];
+  brand?: string[];
+  stock?: number;
+  images?: string[];
+  thumbnail?: { '128x128': string, '256x256': string, '512x512': string };
+}
